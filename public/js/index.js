9b55d160404be4e11d58210edc24241a2c48e7d0
yasevplaton <yasevplaton@gmail.com>
2019-02-04 13:00:08 +0300

    delete console.log

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
 onload = () => {
   if ( window.innerWidth <= 480 ) {
-    let revContentArr = document.querySelectorAll('.reviews__content');
 
-    console.log(revContentArr);
+    let revContentArr = document.querySelectorAll('.reviews__content');
 
     for (let item of revContentArr) {
       item.insertAdjacentHTML('beforeend', '<button type="button" class="btn reviews__btn-mobile">Читать отзыв</button>');
     }
+    
   }
 }
