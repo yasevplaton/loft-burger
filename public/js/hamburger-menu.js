35e2663e1246028f60fe83b23af0a6b96e9d32de
yasevplaton <yasevplaton@gmail.com>
2019-02-09 17:39:52 +0300

    fix inactive nav links

--- public/js/hamburger-menu.js
+++ public/js/hamburger-menu.js
@@ -4,9 +4,11 @@ const modalNav = document.querySelector('.modal-nav');
 hambMenuIcon.addEventListener('click', () => {
   if (hambMenuIcon.classList.contains('close')) {
     hambMenuIcon.classList.remove('close');
+    modalNav.style.right = '9999px';
     modalNav.style.opacity = 0;
   } else {
     hambMenuIcon.classList.add('close');
+    modalNav.style.right = 0;
     modalNav.style.opacity = 0.99;
   }
 });
