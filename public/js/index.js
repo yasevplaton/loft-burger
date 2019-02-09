8441535859469bd19832bfc54b8381347ab972d9
yasevplaton <yasevplaton@gmail.com>
2019-02-09 15:50:08 +0300

    show all reviews on mini-mobiles

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,36 @@
 onLoad = () => {
   const revContentArr = document.querySelectorAll('.reviews__content');
+  addOrRemoveReviewButton(revContentArr);
 
-  // add resize listener on window
-  window.addEventListener('resize', e => {
+  window.addEventListener('resize', () => {
+    addOrRemoveReviewButton(revContentArr);
+  });
+}
 
-    // check if review button for mobiles exists
-    const isReviewButton = document.querySelector('.reviews__btn-mobile');
+function addOrRemoveReviewButton(revContentArr) {
+  // check if review button for mobiles exists
+  const isReviewButton = document.querySelector('.reviews__btn-mobile');
 
-    // if window width is less than or equal to 480px and reviews buttons don't exist yet, add them to DOM
-    if (window.innerWidth <= 480) {
+  // if window width is less than or equal to 480px and reviews buttons don't exist yet, add them to DOM
+  if (window.innerWidth <= 480) {
 
-      if (!isReviewButton) {
-  
-        for (let item of revContentArr) {
-          item.insertAdjacentHTML('beforeend', '<button type="button" class="btn reviews__btn-mobile">Читать отзыв</button>');
-        }
-      }
-    
-    // if window width is more than 480px and reviews buttons exist, remove them from DOM
-    } else {
+    if (!isReviewButton) {
 
-      if (isReviewButton) {
-
-        for (let item of revContentArr) {
-          const reviewButton = item.querySelector('.reviews__btn-mobile');
-          item.removeChild(reviewButton);
-        }
-
+      for (let item of revContentArr) {
+        item.insertAdjacentHTML('beforeend', '<button type="button" class="btn reviews__btn-mobile">Читать отзыв</button>');
       }
     }
-  });
+
+    // if window width is more than 480px and reviews buttons exist, remove them from DOM
+  } else {
+
+    if (isReviewButton) {
+
+      for (let item of revContentArr) {
+        const reviewButton = item.querySelector('.reviews__btn-mobile');
+        item.removeChild(reviewButton);
+      }
+
+    }
+  }
 }
