e6aa9e893fd8ec99e61de27f9f6e77c94d368c87
yasevplaton <yasevplaton@gmail.com>
2019-02-10 16:43:31 +0300

    add animation to modal nav; add some styles to burgers section

--- public/js/hamburger-menu.js
+++ public/js/hamburger-menu.js
@@ -1,14 +1,20 @@
 const hambMenuIcon = document.querySelector('.hamburger-menu');
 const modalNav = document.querySelector('.modal-nav');
+const hamburgerAnimationTime = 300;
 
 hambMenuIcon.addEventListener('click', () => {
+
   if (hambMenuIcon.classList.contains('close')) {
+
     hambMenuIcon.classList.remove('close');
-    modalNav.style.right = '9999px';
     modalNav.style.opacity = 0;
+    setTimeout(() => modalNav.style.right = "9999px", hamburgerAnimationTime);
+
   } else {
+
     hambMenuIcon.classList.add('close');
     modalNav.style.right = 0;
     modalNav.style.opacity = 0.98;
+    
   }
 });
