6a66ea480c6608227cc62c38e70c53acdece4589
yasevplaton <yasevplaton@gmail.com>
2019-02-12 19:08:04 +0300

    add anchors; close modal nav by click on nav item and escape key

--- public/js/hamburger-menu.js
+++ public/js/hamburger-menu.js
@@ -1,20 +1,46 @@
 const hambMenuIcon = document.querySelector('.hamburger-menu');
 const modalNav = document.querySelector('.modal-nav');
+const navItems = modalNav.querySelectorAll('.modal-nav__item');
 const hamburgerAnimationTime = 300;
 
+// add click listener to hamburger icon
 hambMenuIcon.addEventListener('click', () => {
 
   if (hambMenuIcon.classList.contains('close')) {
 
-    hambMenuIcon.classList.remove('close');
-    modalNav.style.opacity = 0;
-    setTimeout(() => modalNav.style.right = "9999px", hamburgerAnimationTime);
+    closeModalNav();
 
   } else {
 
-    hambMenuIcon.classList.add('close');
-    modalNav.style.right = 0;
-    modalNav.style.opacity = 0.98;
+    openModalNav();
     
   }
 });
+
+// close modal nav when user select navigation item
+Array.from(navItems).forEach(item => {
+  item.addEventListener('click', () => {
+    closeModalNav();
+  });
+});
+
+// close modal nav when user clicks escape key
+document.body.addEventListener('keydown', e => {
+  if (e.keyCode == 27) { 
+    closeModalNav();
+  } 
+});
+
+
+// functions to open and close modal navigation
+function openModalNav() {
+  hambMenuIcon.classList.add('close');
+  modalNav.style.right = 0;
+  modalNav.style.opacity = 0.98;
+}
+
+function closeModalNav() {
+  hambMenuIcon.classList.remove('close');
+  modalNav.style.opacity = 0;
+  setTimeout(() => modalNav.style.right = "9999px", hamburgerAnimationTime);
+}
